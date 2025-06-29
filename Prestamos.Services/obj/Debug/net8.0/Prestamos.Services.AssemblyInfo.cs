7aca61959b1c35f01f42a86c03cb70a927c3bbc0
Gloria Fulguera <gloriafulguera@gmail.com>
2025-06-29 20:02:27 -0300

    referencias

--- Prestamos.Services/obj/Debug/net8.0/Prestamos.Services.AssemblyInfo.cs
+++ Prestamos.Services/obj/Debug/net8.0/Prestamos.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prestamos.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b42253c77f640e68316a73158b9e65784008021")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prestamos.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prestamos.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
